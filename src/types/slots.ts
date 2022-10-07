40d9292e947150b4a59be2317551ffc9ec9aa55a
Arjun <arjun140702@gmail.com>
2022-10-08 03:57:49 +0530

    Improvize all slots data returned

---
+++ src/types/slots.ts
@@ -0,0 +1,3 @@
+type Slots = { [key: string]: { start: string; end: string } }
+
+export default Slots
--- src/utils/parseTimetable.ts
+++ src/utils/parseTimetable.ts
@@ -1,12 +1,16 @@
 import type Course from "../types/course"
+import type Slots from "../types/slots"
 
 const parseTimetable = (
     str: string,
     allSlots?: boolean
-): Course[] | { courses: Course[]; slots: { [key: string]: string[] } } => {
+): {
+    courses: Course[]
+    slots?: Slots
+} => {
     const data: string[] = []
     const courses: Course[] = []
-    const slots: { [key: string]: string[] } = {}
+    const slots: Slots = {}
 
     for (const s of str.split("\n")) {
         //@ts-ignore
@@ -21,36 +25,36 @@ const parseTimetable = (
     for (var i = 0; i < data.length; i += 2) {
         for (var j = 2; j < data[i].length; j++) {
             const lec = data[i][j].split("-")
+            const [start, end] = [theoryStart![j], theoryEnd![j - 1]]
 
             if (allSlots && data[i][j] !== "-" && data[i][j] !== "Lunch")
-                slots[lec[0]] = []
+                slots[lec[0]] = { start, end }
 
             if (data[i][j] !== "-" && lec.length > 1) {
                 const [slot, code, type, room] = lec
-                const [start, end] = [theoryStart![j], theoryEnd![j - 1]]
                 courses.push({ slot, code, type, room, start, end })
             }
         }
 
         for (var j = 1; j < data[i + 1].length; j++) {
             const lec = data[i + 1][j].split("-")
+            const [start, end] = [labStart![j], labEnd![j - 1]]
 
             if (
                 allSlots &&
                 data[i + 1][j] !== "-" &&
                 data[i + 1][j] !== "Lunch"
             )
-                slots[lec[0]] = []
+                slots[lec[0]] = { start, end }
 
             if (data[i + 1][j] !== "-" && lec.length > 1) {
                 const [slot, code, type, room] = lec
-                const [start, end] = [labStart![j], labEnd![j - 1]]
                 courses.push({ slot, code, type, room, start, end })
             }
         }
     }
 
-    return allSlots ? { courses, slots } : courses
+    return allSlots ? { courses, slots } : { courses }
 }
 
 export default parseTimetable
