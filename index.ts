a00e0a6246310333f2f40315b6c42dd91e6548ee
Arjun <arjun140702@gmail.com>
2022-10-08 05:16:36 +0530

    Add exports

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import findFreeSlots from "./src/utils/findFreeSlots"
+import parseTimetable from "./src/utils/parseTimeTable"
 
 const s = ``
 
 console.log(findFreeSlots([s]))
+
+export { findFreeSlots, parseTimetable }
+export default findFreeSlots
