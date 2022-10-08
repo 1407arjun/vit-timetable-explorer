23218165c7b082bde5cc2b60397c2dc891d32726
Arjun <arjun140702@gmail.com>
2022-10-08 15:57:21 +0530

    Bump version

--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
 import findFreeSlots from "./src/utils/findFreeSlots"
 import parseTimetable from "./src/utils/parseTimeTable"
 
-const s = ``
-
-console.log(findFreeSlots([s]))
-
 export { findFreeSlots, parseTimetable }
 export default findFreeSlots
